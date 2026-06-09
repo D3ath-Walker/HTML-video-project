197298adcb60b79cc7e0d03bebd994501744befb
D3ath-Walker <razor7903@gmail.com>
2026-06-09 08:36:47 +0530

    front page added

---
+++ Frontend/js/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("student-form");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const course = document.getElementById("course").value;
+    const college = document.getElementById("college").value;
+
+    localStorage.setItem("name", name);
+    localStorage.setItem("course", course);
+    localStorage.setItem("college", college);
+
+    window.location.href = "dashboard.html";
+});
