209529c3229efb76c6884458030178869538216e
D3ath-Walker <razor7903@gmail.com>
2026-06-15 09:02:16 +0530

    db

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,41 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+// Schema FIRST
+const StudentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: String,
+        required: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    joinedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Model AFTER schema
+const Student = mongoose.model("Student", StudentSchema);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
     res.send("Server Running");
